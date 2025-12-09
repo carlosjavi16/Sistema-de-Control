4324dee30df2e0f80499dea7c3c07f9c0456d811
spyke <carjavi11bta@gmail.com>
2025-12-09 03:57:41 -0400

    Reordenar codigo, mejorar mensajes y permitir salida controlada por el usuario

--- pruebacommit.c
+++ pruebacommit.c
@@ -1,28 +1,39 @@
 //Oliver Garcia C.I:32.396.121
 //Luis Lira C.I.:31.564.286
 // Fabiola Andrade C.I.:31.209.874
+// Carlos Belmonte C.I:31.722.091
 #include <stdio.h>
 int main(){
 int fortuna=0;
 
+printf("\tADIVINA TU FORTUNA");
+printf("\n\nIngresa un numero del 1 al 3 (0 para salir):");
 do{
-printf("ADIVINA TU FORTUNA\n");
-printf("\tSeleccione un numero\n");
+
 scanf("%d",&fortuna);
-switch(fortuna)
-{
+
+switch(fortuna){
+case 0:
+    printf("\n\nHasta luego!\n");
+    break;
 case 1:
-    printf("Su fortuna es aprender a usar switch y case\n");
+    printf("\nSu fortuna sera aprender a usar switch y case\n");
     break;
-    case 2:
-    printf("Su fortuna es econtrarse 100$ en la calle\n");
+case 2:
+    printf("\nAlgo me dice que encontraras 100$ en la calle\n");
     break;
-    case 3:
-    printf("Su fortuna es que hoy es un buen dia para culpar a un tercero. Si falla tu codigo, fue el sistema operativo.");
+case 3:
+    printf("\nHoy es un buen dia para culpar a un tercero. Si falla tu codigo, fue el sistema operativo.");
     break;
 default:
-    printf("sea serio ombe pon la vaina como eh!!\n");
+    printf("\nSea serio ombe pon la vaina como eh!!\n");
     }
-}while(fortuna<1||fortuna>7);
+
+if(fortuna!=0){
+    printf("\n\nOtro Numero (0 para salir):");
+    }
+
+}while(fortuna!=0);
+
 return 0;
 }
