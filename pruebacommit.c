935480bcab167317eca39b8b6a004694d8b227f5
Fabiola Andrade <fabiolaspienza16@gmail.com>
2025-12-09 02:36:54 -0400

    Agrege un switcase

--- pruebacommit.c
+++ pruebacommit.c
@@ -1,19 +1,24 @@
 //Oliver Garcia C.I:32.396.121
 //Luis Lira C.I.:31.564.286
-#include<stdio.h>
+// Fabiola Andrade C.I.:31.209.874
+#include <stdio.h>
 int main(){
 int fortuna=0;
 
 do{
+printf("ADIVINA TU FORTUNA\n");
 printf("\tSeleccione un numero\n");
 scanf("%d",&fortuna);
 switch(fortuna)
 {
 case 1:
-    printf("su fortuna es aprender a usar switch y case\n");
+    printf("Su fortuna es aprender a usar switch y case\n");
     break;
     case 2:
-    printf("su fortuna es econtrarse 100$ en la calle\n");
+    printf("Su fortuna es econtrarse 100$ en la calle\n");
+    break;
+    case 3:
+    printf("Su fortuna es que hoy es un buen dia para culpar a un tercero. Si falla tu codigo, fue el sistema operativo.");
     break;
 default:
     printf("sea serio ombe pon la vaina como eh!!\n");
