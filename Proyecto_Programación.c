6adf0fc4fe219723d49e5b0677eca9b0c5af2a16
NanoSVz <carlosluisruizolmos@gmail.com>
2026-01-30 18:01:49 -0400

    Registro de estudiantes con nota Carlos Ruiz 30.663.314
    Estructura del estudiante para la creación de la lista
    Creación del registro de estudiantes
    Librerías Agregadas
    Corrección de sintaxis y llaves

--- Proyecto_Programación.c
+++ Proyecto_Programación.c
@@ -1,7 +1,20 @@
 //khristian lima 28.372.514
+//Carlos Ruiz 30.663.314
+
 #include <iostream>
 #include <limits>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cctype>
+
 using namespace std;
+
+struct Estudiante {
+    string nombre, apellido;
+	float nota; 
+    bool presente; // true = Asistió, false = Faltó
+};
 
 int main(){
 	int numeroEstudiantes;
@@ -16,15 +29,42 @@ int main(){
 			cout << "\nPor favor ingrese una opcion valida (S o N): ";
             cin >> opcion;
 		}
-if(opcion=='S'||opcion=='s'){
+	if(opcion=='S'||opcion=='s'){
 	
-	cout<<"\n¿Cuantos estudiantes desea ingresar al sistema? (indique el numero de estudiantes):";
+		cout<<"\n¿Cuantos estudiantes desea ingresar al sistema? (indique el numero de estudiantes):";
 	
-while(!(cin>>numeroEstudiantes)||numeroEstudiantes < 0){
+	while(!(cin>>numeroEstudiantes)||numeroEstudiantes < 0){
 		count<<"\nERROR:Por favor no ingresar letras ni numeros negativos. Ingrese nuevamente el numero:";
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
+	}
+	vector<Estudiante> lista(numeroEstudiantes); //Se utiliza para crear una lista dinámica de estudiantes basada en la cantidad ingresada.
+		
+		// Registro de estudiantes y sus notas.
+		for(int i = 0; i < numeroEstudiantes; i++){
+			
+			cout << "\nIngrese el nombre del estudiante numero " << i+1 << ": ";
+			cin >> lista[i].nombre;
+			cout << "Ingrese el apellido del estudiante numero " << i+1 << ": ";
+			cin >> lista[i].apellido;
+			
+			cout << "El estudiante esta asistio? S/N: ";
+			cin >> asistencia;
+			while(asistencia != 's' && asistencia != 'S' && asistencia != 'n' && asistencia != 'N'){
+			
+				cout << "\nPor favor ingrese una opcion valida (S o N): ";
+           		cin >> asistencia;
+			}
+			lista[i].presente = (asistencia == 's' || asistencia == 'S');
+			
+			cout << "Ingrese la nota del estudiante numero " << i+1 << ": ";
+			while(!(cin >> lista[i].nota) || lista[i].nota > 20 || lista[i].nota < 0){
+				
+				cout<<"\nERROR: Por favor coloque una nota entre 0 y 20, sin letras. Ingrese nuevamente la nota: ";
+				cin.clear();
+            	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			}
+		}
 }else{
 	cout<<"\n\nHasta luego, que tenga buen dia";
 }
