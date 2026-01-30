f5680ebbc3d4c4835c71a5727007d9b1c3876072
khristianlima41-lab <khristianlima41@gmail.com>
2026-01-30 17:32:53 -0400

    Numero de estudiantes y correci
    añado confirmación de s/n y tomo el número de estudiantes, también me aseguré de que no haya errores si el usuario ingresa letras un números negativos para que el programa solo procese lo que quiero

--- Proyecto_Programación.c
+++ Proyecto_Programación.c
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include <limits>
 using namespace std;
 
 int main(){
+	int numeroEstudiantes;
 	char opcion;
 	
 	cout << "\tSistema de Control de estudiantes en un Taller" << endl;
@@ -14,6 +15,17 @@ int main(){
 			cout << "\nPor favor ingrese una opcion valida (S o N): ";
             cin >> opcion;
 		}
-
+if(opcion=='S'||opcion=='s'){
+	
+	cout<<"\n¿Cuantos estudiantes desea ingresar al sistema? (indique el numero de estudiantes):";
+	
+while(!(cin>>numeroEstudiantes)||numeroEstudiantes < 0){
+		count<<"\nERROR:Por favor no ingresar letras ni numeros negativos. Ingrese nuevamente el numero:";
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+}else{
+	cout<<"\n\nHasta luego, que tenga buen dia";
+}
 return 0;
 }
