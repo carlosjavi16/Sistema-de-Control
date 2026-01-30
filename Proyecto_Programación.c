aff09a5dddf1174a20f8b356d1379f518eb1c16a
keiber-05 <keiberacero@gmail.com>
2026-01-30 16:11:15 -0400

    Mensaje de bienvenida-Keiber_Acero CI:32.589.206
    Mensaje de bienvenida
    Ingreso de estudiantes

--- Proyecto_Programación.c
+++ Proyecto_Programación.c
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+	char opcion;
+	
+	cout << "\tSistema de Control de estudiantes en un Taller" << endl;
+	cout <<"\nNo se poseen datos de estudiantes, desea ingresarlos? S/N: ";
+	cin >> opcion;
+	
+	while(opcion != 's' && opcion != 'S' && opcion != 'n' && opcion != 'N'){
+			
+			cout << "\nPor favor ingrese una opcion valida (S o N): ";
+            cin >> opcion;
+		}
+
+return 0;
+}
